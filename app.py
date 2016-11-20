0145e57d6c7687280c94128db1b96d754ab09a1b
Michael Dresser <michaelmdresser@gmail.com>
2016-11-19 18:49:59 -0700

    source files rename, deletion of unused txt files, removed prefacingtext functionality

--- app.py
+++ app.py
@@ -16,38 +16,24 @@ def is_int(input):
 app = Flask(__name__)
 CORS(app)
 
-setOfPrefacingTemplates = [
-  "Good question: %(issue)s is a contentious issue. Here's what Obama says:",
-  "%(issue)s, eh. He was hoping you'd ask that.",
-  "He normally doesn't like to talk about %(issue)s, but he'll make an exception this one time.",
-  "I mean is %(issue)s really that relevant? Obama will make an exception just this once.",
-  "He has his own personal opinions about %(issue)s, but within his capacity as President, here's what he thinks:"
-]
-
 @app.route('/chat', methods=["GET", "OPTIONS"])
 @cross_origin()
 def index():
 
-  prefacingText = ""
   size = ""
-
-  if request.args.get('issue') != None:
-    issue = unicode(request.args.get('issue'))
-    prefacingText = (setOfPrefacingTemplates[random.randint(0, len(setOfPrefacingTemplates) - 1)] % locals())
 
   if request.args.get('size') != None:
     size = unicode(request.args.get('size'))
 
   newSpeech = ""
 
-  with open('allspeeches.txt') as f:
+  with open('source.txt') as f:
     rawText = f.read()
 
   model = markovify.Text(rawText, state_size=3)
 
   if size == "":
     for i in range(2):
-      #newSpeech += " " + model.make_short_sentence(140)
       newSpeech += " " + model.make_sentence()
   elif size == "tweet":
     newSpeech += " " + model.make_short_sentence(140)
@@ -56,8 +42,7 @@ def index():
       newSpeech += " " + model.make_sentence()
 
   return jsonify({
-    "content": newSpeech,
-    "prefacingText": prefacingText
+    "content": newSpeech
   })
 
 if __name__ == "__main__":
